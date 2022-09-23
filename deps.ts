e3d57b75cffb99a159c0fd0ebcb7af829c367bdc
Plat <60182057+p1atdev@users.noreply.github.com>
2022-09-23 13:41:59 +0900

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,7 @@
+// Deno's standard modules are here: https://deno.land/std
+
+// server
+export * from "https://deno.land/std@0.152.0/http/server.ts";
+
+// test
+export * from "https://deno.land/std@0.152.0/testing/asserts.ts";
